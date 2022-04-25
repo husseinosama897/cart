41e198db120f93117be7f3dd217d760c674f3a5e
husseinosama897 <95488225+husseinosama897@users.noreply.github.com>
2022-04-25 13:27:21 +0200

    update

--- cart/routes/web.php
+++ cart/routes/web.php
@@ -16,3 +16,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('genrate-sitemap', function(){
+
+    // create new sitemap object
+    $sitemap = App::make("sitemap");
+
+    // add items to the sitemap (url, date, priority, freq)
+    $sitemap->add(URL::to('home'), '2012-08-25T20:10:00+02:00', '1.0', 'daily');
+    $sitemap->add(URL::to('category'), '2012-08-26T12:30:00+02:00', '0.9', 'monthly');
+
+    // get all posts from db
+    $categories = category::all();
+
+    // add every post to the sitemap
+    foreach ($categories as $category)
+    {
+        $sitemap->add(URL::to('categories/'.$category->id), $category->updated_at, '1.0', 'daily');
+    }
+
+    // generate your sitemap (format, filename)
+    $sitemap->store('xml', 'sitemap');
+    // this will generate file mysitemap.xml to your public folder
+
+    return redirect(url('sitemap.xml'));
+});
