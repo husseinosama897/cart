5032bae39be711ee6c0f8a393d5b1ef5d7ebbcdf
husseinosama897 <95488225+husseinosama897@users.noreply.github.com>
2022-05-10 16:56:04 +0200

    packaging

--- cart/app/Http/Controllers/packingController.php
+++ cart/app/Http/Controllers/packingController.php
@@ -17,11 +17,63 @@ class packingController extends Controller
             'name'=>['required','string','max:255'],
         ]);
 $data = product::where('name', 'LIKE', '%' . $request->name . '%')
-->select(['name','image','type','id'])->where('type',1)->get()->take(5);
+->select(['name','image','price','type','id'])->where('type',2)->get()->take(5);
 
 return response()->json(['data'=>$data]);
     }
 
 
-    
+    public function insertcup(request $request){
+$data =  json_decode($request->packing,true);
+$rules = [
+              
+           
+    'id' => 'required|exists:products,id',
+    'image'=>[ 'image|mimes:jpeg,png,jpg,gif,svg|max:2048']
+];
+
+$insrting = [];
+
+foreach($data as $dat){
+
+    $validator = Validator::make($dat,
+
+        $rules
+
+     );
+    if ($validator->passes()) {
+
+
+        if($dat['image']){
+            $image_tmp = $dat['image'];
+                $extension = $image_tmp->getClientOriginalExtension();
+                $fileName = rand(111,99999).'.'.$extension;
+                $image_tmp->move('uploads/attachment', $fileName);
+          }else{
+            $fileName = null;
+          }
+
+$insrting[
+
+]= [
+    'product_id'=>$dat['id'],
+    'price'=>$dat['price'],
+    'quantity'=>$dat['quantity'],
+    'total'=>$dat['quantity'] *  $dat['price'],
+    'image'=>$fileName,
+
+];
+
+    }
+
+}
+$chunkcup = array_chunk($insrting, 10);
+
+foreach($chunkcup as $cup){
+    cart::insert($cup);
+}
+
+
+    }
+
 }
