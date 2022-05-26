232ca7cd3a73e92318fabae7b9c19fe9aaa798d9
husseinosama897 <95488225+husseinosama897@users.noreply.github.com>
2022-05-26 04:11:07 +0200

    data

--- cart/app/Http/Controllers/profileController.php
+++ cart/app/Http/Controllers/profileController.php
@@ -9,11 +9,11 @@ class profileController extends Controller
     public function profile(){
 
         $data = auth()->user()->with(['order'=>function($q){
-            
+
 return $q->with('itemorder');
         
 }]);
 
-        return view('front.profile');
+        return view('front.profile')->with(['data'=>$data]);
     }
 }
