05ed1d6baf6c9e88ffbb6a9f4b615cc25b0a1448
husseinosama897 <95488225+husseinosama897@users.noreply.github.com>
2022-06-07 10:52:06 +0200

    update

--- cart/app/Http/Controllers/couponController.php
+++ cart/app/Http/Controllers/couponController.php
@@ -3,8 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Model\coupon;
 class couponController extends Controller
 {
-    //
+    public function createpage(){
+        return view('admin.coupon.create');
+    }
+
+
+
+    public function post(request $request){
+        $this->validate($request,[
+            'code'=>['required','max:255','string'],
+            'type'=>['required','string'],
+            'value'=>['numeric'],
+            'percent_off'=>['numeric'],
+            'expire'=>['string'],
+        ]);
+
+        $coupon = coupon::create([
+            'code'=>$request->code,
+            'type'=>$request->type, // voucher || percent_off 
+            'value'=>$request->value,
+            'percent_off'=>$request->percent_off,
+            'expire'=>$request->expire,
+        ]);
+
+
+        
+    }
+
+    public function updatepage(request $request,coupon $coupon){
+
+    return view('admin.coupon.update')->with('coupon',$coupon);
+        
+    }
+
+    public function postupdate(request $request)
 }
